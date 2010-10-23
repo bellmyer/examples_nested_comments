c30a334b7e5f97ce33f6b053465dad7b1fee0dd5
Jaime Bellmyer <online@bellmyer.com>
2010-10-23 11:24:37 -0500

    added post's comment associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101023161650) do
+ActiveRecord::Schema.define(:version => 20101023161744) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id", "commentable_type"], :name => "index_comments_on_commentable_id_and_commentable_type"
 
   create_table "posts", :force => true do |t|
     t.string   "title"
