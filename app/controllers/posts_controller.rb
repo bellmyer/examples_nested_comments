37b91cfee64044e6a66727c96fe6e02f09aedbba
Jaime Bellmyer <online@bellmyer.com>
2010-11-21 10:27:06 -0600

    added posts scaffold

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,38 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(params[:post])
+    
+    if @post.save
+      redirect_to posts_path, :notice => "Your post was created successfully."
+    else
+      render :action => :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    @post.update_attributes(params[:post])
+    redirect_to post_path(@post), :notice => 'Your post was updated successfully.'
+  end
+  
+  def destroy
   end
 
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,97 @@
 require 'spec_helper'
 
-describe PostsController do
+def mock_post(stubs={})
+  (@mock_post ||= mock_model(Post).as_null_object).tap do |post|
+    post.stub(stubs) unless stubs.empty?
+  end
+end
 
+describe PostsController do
+  describe "GET index" do
+    before do
+      Post.should_receive(:all).and_return([mock_post])
+      get :index
+    end
+    
+    it { should assign_to(:posts).with([mock_post])}
+    it { should respond_with :success }
+    it { should render_template :index }
+    it { should_not set_the_flash }
+  end
+  
+  describe "GET show" do
+    before do
+      Post.should_receive(:find).with('1001').and_return(mock_post)
+      get :show, :id => '1001'
+    end
+    
+    it { should assign_to(:post).with(mock_post) }
+    it { should respond_with :success }
+    it { should render_template :show }
+    it { should_not set_the_flash }
+  end
+  
+  describe "GET new" do
+    before do
+      Post.should_receive(:new).and_return(mock_post)
+      get :new
+    end
+    
+    it { should assign_to(:post).with(mock_post) }
+    it { should respond_with :success }
+    it { should render_template :new }
+    it { should_not set_the_flash }
+  end
+  
+  describe "POST create" do
+    describe "with valid data" do
+      before do
+        Post.should_receive(:new).with('these' => 'params').and_return(mock_post(:save => true))
+        post :create, :post => {'these' => 'params'}
+      end
+      
+      it { should assign_to(:post).with(mock_post) }
+      it { should redirect_to posts_path }
+      it { should set_the_flash.to /success/i }
+    end
+    
+    describe "with invalid data" do
+      before do
+        Post.should_receive(:new).with('these' => 'params').and_return(mock_post(:save => false))
+        post :create, :post => {'these' => 'params'}
+      end
+      
+      it { should assign_to(:post).with(mock_post) }
+      it { should respond_with :success }
+      it { should render_template :new }
+      it { should_not set_the_flash }
+    end
+  end
+  
+  describe "GET edit" do
+    before do
+      Post.should_receive(:find).with('1001').and_return(mock_post)
+      get :edit, :id => '1001'
+    end
+    
+    it { should assign_to(:post).with(mock_post) }
+    it { should respond_with :success }
+    it { should render_template :edit }
+    it { should_not set_the_flash }
+  end
+  
+  describe "PUT update" do
+    describe "with valid data" do
+      before do
+        Post.should_receive(:find).with('1001').and_return(mock_post)
+        mock_post.should_receive(:update_attributes).with('these' => 'params').and_return(true)
+        
+        put :update, :id => '1001', :post => {'these' => 'params'}
+      end
+      
+      it { should assign_to(:post).with(mock_post) }
+      it { should redirect_to post_path(mock_post) }
+      it { should set_the_flash.to /success/i }
+    end
+  end
 end
